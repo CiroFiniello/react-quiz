53732cc4777770876b1482b986cdfd0ea1a75059
CiroFiniello <kirofiniello@outlook.it>
2024-11-21 01:29:37 +0100

    ho importato le domande come fossero un'api, direttamente al mount dell'app attraverso uno useEffect, e le sue relative funzioni

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "./Header.jsx";
 import Mainn from "./Mainn.jsx";
 import "./App.css";
 
 function App() {
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error("Error"));
+  }, []);
   return (
     <div className="app">
       <Header />
