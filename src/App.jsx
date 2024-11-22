deb09a81458aab21529d183378f8e033cbe235c7
CiroFiniello <kirofiniello@outlook.it>
2024-11-22 18:11:30 +0100

    ho aggiunto le domande al file, caricate come api esterne, ne ho permesso la lettura , e ho infine utilizzato il reducer per prevedere le dispatch per i relativi risultati "data recived" e "data failed" inoltre ho settato un caso di default, tramite il reducer viene preso il vecchio stato  come argomento e l'azione,  ne consegue un'azione di aggiornamento sullo stato, utilizzando lo statemant switch

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import Header from "./Header.jsx";
 import Mainn from "./Mainn.jsx";
 import "./App.css";
+const initialState = {
+  questions: [],
 
+  //loading, error,  ready , active , finished
+  status: "loading",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Action unkonwn");
+  }
+}
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.error("Error"));
+      .then((data) => dispatch({ type: "dataRecived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
   }, []);
   return (
     <div className="app">
